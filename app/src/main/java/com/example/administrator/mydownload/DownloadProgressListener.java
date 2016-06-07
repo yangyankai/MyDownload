62ab987dd3c28fecbd6e3f80a5dcda73ff7b379f
ykai <yangyankai_mr@outlook.com>
2016-06-07 21:43:56 +0800

    多线程断点下载。

---
+++ app/src/main/java/com/example/administrator/mydownload/DownloadProgressListener.java
@@ -0,0 +1,11 @@
+package com.example.administrator.mydownload;
+
+/*******************************************************************************
+ * Author  : Yankai
+ * Date    : 2016-05-17 18:16
+ * Email   : yk_yang@wesugarfree.com
+ * Company : 上海无糖运动
+ ******************************************************************************/
+public interface DownloadProgressListener {
+    public void onDownloadSize(int size);
+}
